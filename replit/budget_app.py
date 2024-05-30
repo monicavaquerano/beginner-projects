62dbc300a64bf7c722c172556dce52c04c5e2b7f
monicavaquerano <106926593+monicavaquerano@users.noreply.github.com>
2024-05-30 20:53:36 +0200

    update budget_app.py

--- replit/budget_app.py
+++ replit/budget_app.py
@@ -134,7 +134,6 @@ if __name__ == "__main__":
     entertainment.withdraw(33.40)
     business.withdraw(10.99)
 
-    solution = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
-
-    print(solution)
+    # solution = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+    # print(solution)
     print((create_spend_chart([business, food, entertainment])))
